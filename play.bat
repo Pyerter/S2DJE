0b8c400210ebdf8028af50578da722051a1e15b9
Jonathan Porter Squires <57535724+jps70135@users.noreply.github.com>
2019-12-16 15:38:38 -0500

    Create play.bat

---
+++ play.bat
@@ -0,0 +1,21 @@
+@echo off
+
+SET prefix=Sharp$
+
+:: run the main java file
+echo %prefix% Running sharp.game.Driver (launch App)!
+java -cp bin\java\ sharp.game.Driver
+
+:: if there was an error, go to the runerr statement
+if errorlevel 1 goto:runerr
+echo %prefix% Runtime successful!
+
+echo %prefix%
+
+pause
+goto:EOF
+
+:runerr
+echo %prefix% Runtime unsuccessful
+pause
+goto:EOF
