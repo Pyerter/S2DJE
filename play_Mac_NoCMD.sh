36e0b87e575f95a4f0cd882b4b869dd42c385332
Jonathan Porter Squires <57535724+jps70135@users.noreply.github.com>
2019-12-22 15:23:12 -0500

    Changed mac play script to do no output

--- play_Mac_NoCMD.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ex
-
-java -Xmx256m -prism.order=sw -cp $(pwd)/bin/java/ sharp.game.Driver "os: mac" "output: false"
--- scifirpgMacPlayShellScript.sh
+++ scifirpgMacPlayShellScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
-java -Xmx256m -cp $(pwd)/bin/java/ sharp.game.Driver "os: mac"
+java -Xmx256m -cp $(pwd)/bin/java/ sharp.game.Driver "os: mac" "output: false"
 
